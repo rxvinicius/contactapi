b8d06f6fe8556372e3607b1476a902c3fefa8685
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-06-10 17:33:14 -0300

    Initial commit

---
+++ src/main/java/io/rxvinicius/contactapi/Application.java
@@ -0,0 +1,13 @@
+package io.rxvinicius.contactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/io/rxvinicius/contactapi/service/ContactService.java
@@ -0,0 +1,78 @@
+package io.rxvinicius.contactapi.service;
+
+import io.rxvinicius.contactapi.constants.Contants;
+import io.rxvinicius.contactapi.domain.Contact;
+import io.rxvinicius.contactapi.repo.ContactRepo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepo contactRepo;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContactById(String id) {
+        return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContactById(String id) {
+        contactRepo.deleteById(id);
+    }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        log.info("Uploading photo for user ID: {}", id);
+        Contact contact = getContactById(id);
+        String photoUrl = photoFunction.apply(id, file);
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = fileName -> {
+        return Optional.of(fileName)
+                .filter(f -> f.contains("."))
+                .map(f -> "." + f.substring(fileName.lastIndexOf(".") + 1)).orElse(".png");
+    };
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String fileName = id + fileExtension.apply(image.getOriginalFilename());
+
+        try {
+            Path fileStorageLocation = Paths.get(Contants.PHOTO_DIRECTORY).toAbsolutePath().normalize();
+
+            if (Files.notExists(fileStorageLocation)) {
+                Files.createDirectories(fileStorageLocation);
+            }
+
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(fileName), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder.fromCurrentContextPath().path("/contacts/image" + fileName).toUriString();
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    };
+}
