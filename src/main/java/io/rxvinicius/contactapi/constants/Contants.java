7fa6f05c1db33e968d92f27606e7ffcaf29aead5
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-06-12 11:12:00 -0300

    Cors configuration

--- src/main/java/io/rxvinicius/contactapi/constants/Contants.java
+++ src/main/java/io/rxvinicius/contactapi/constants/Contants.java
@@ -2,4 +2,5 @@ package io.rxvinicius.contactapi.constants;
 
 public class Contants {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
